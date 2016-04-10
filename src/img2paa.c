1158cc88b487a29de8b0d7612062972a9699c192
KoffeinFlummi <koffeinflummi@gmail.com>
2016-04-10 22:46:16 +0200

    Revert "Temporarily delete the paa stuff"
    This reverts commit ccac6407634c3e633c563c8eddfa72e4fe3d2bce.

---
+++ src/img2paa.c
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C)  2016  Felix "KoffeinFlummi" Wiegand
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#include "minilzo.h"
+
+#include "docopt.h"
+#include "img2paa.h"
+
+
+int img2dxt1(unsigned char *input, unsigned char *output, int width, int height) {
+    return 0;
+}
+
+
+int img2dxt3(unsigned char *input, unsigned char *output, int width, int height) {
+    return 0;
+}
+
+
+int img2dxt5(unsigned char *input, unsigned char *output, int width, int height) {
+    return 0;
+}
+
+
+int img2paa(DocoptArgs args) {
+    return 0;
+}
--- src/main.c
+++ src/main.c
@@ -23,6 +23,8 @@
 #include <string.h>
 
 #include "docopt.h"
+#include "img2paa.h"
+#include "paa2img.h"
 #include "binarize.h"
 #include "build.h"
 
@@ -55,6 +57,10 @@ int main(int argc, char *argv[]) {
     }
 
 
+    if (args.img2paa)
+        return img2paa(args);
+    if (args.paa2img)
+        return paa2img(args);
     if (args.binarize)
         return binarize(args);
     if (args.build)
