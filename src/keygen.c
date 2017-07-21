1d0d849c91724ba2090cddcdc802391bfd1c42f2
KoffeinFlummi <koffeinflummi@gmail.com>
2017-07-21 16:49:04 +0200

    Fix indentation

--- src/keygen.c
+++ src/keygen.c
@@ -35,12 +35,12 @@
 #if OPENSSL_VERSION_NUMBER < 0x10100000L
 void RSA_get0_key(const RSA *r,
         const BIGNUM **n, const BIGNUM **e, const BIGNUM **d) {
-  if (n != NULL)
-      *n = r->n;
-  if (e != NULL)
-      *e = r->e;
-  if (d != NULL)
-      *d = r->d;
+    if (n != NULL)
+        *n = r->n;
+    if (e != NULL)
+        *e = r->e;
+    if (d != NULL)
+        *d = r->d;
 }
 
 void RSA_get0_factors(const RSA *r, const BIGNUM **p, const BIGNUM **q) {
