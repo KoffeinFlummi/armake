fe58486d858699394368055b8b4f7283fe712a3a
Vincent <agpheaddikilla@yahoo.de>
2018-05-05 15:43:40 +0200

    add new unit test

--- test/signing/test.sh
+++ test/signing/test.sh
@@ -7,6 +7,7 @@ cp test/signing/*.pbo /tmp/amktest/
 
 ./bin/armake sign test/signing/*.biprivatekey /tmp/amktest/ace_fcs.pbo
 ./bin/armake sign test/signing/*.biprivatekey /tmp/amktest/ace_vehiclelock.pbo
+./bin/armake sign test/signing/*.biprivatekey /tmp/amktest/dbo_old_bike.pbo
 
 cmp --silent test/signing/ace_fcs.pbo.*.bisign /tmp/amktest/ace_fcs.pbo.*.bisign || {
     rm -rf /tmp/amktest
@@ -14,6 +15,12 @@ cmp --silent test/signing/ace_fcs.pbo.*.bisign /tmp/amktest/ace_fcs.pbo.*.bisign
 }
 
 cmp --silent test/signing/ace_vehiclelock.pbo.*.bisign /tmp/amktest/ace_vehiclelock.pbo.*.bisign || {
+    rm -rf /tmp/amktest
+    exit 1
+}
+
+sha256sum test/signing/dbo_old_bike.pbo.*.bisign /tmp/amktest/dbo_old_bike.pbo.*.bisign
+cmp --silent test/signing/dbo_old_bike.pbo.*.bisign /tmp/amktest/dbo_old_bike.pbo.*.bisign || {
     rm -rf /tmp/amktest
     exit 1
 }
